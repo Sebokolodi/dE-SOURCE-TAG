9731d7e6a5a8fb7517111db2f6aad8a7977f69ad
Sebokolodi <mll.sebokolodi@gmail.com>
2019-07-20 22:57:15 -0600

    ready to package

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+__version__ = "0.1"
+
+
+scripts = ["bin/" + j for j in os.listdir("bin") ]
+
+requires = [
+    "numpy",
+    "astropy",
+    "Tigger",
+    "argparse"
+]
+
+
+setup(name = "pyddi",
+    version = __version__,
+    description = "Identifies Sources that Require Direction-Dependent Calibration.",
+    author = "Lerato Sebokolodi",
+    author_email = "mll.sebokolodi@gmail.com",
+    url = "https://github.com/Sebokolodi/pyddi",
+    packages = find_packages(),
+    install_requires = requires,
+    scripts = scripts, 
+    license = "GNU GPL v2",
+    classifiers = [],
+ )
