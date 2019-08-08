f506f601464cdbb3574c9ed231f039a885ceb197
Sebokolodi <mll.sebokolodi@gmail.com>
2019-08-07 19:42:58 -0600

    exclude radius, fix x-size to abs(x-size)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 pkg = 'pyddi'
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 build_root = os.path.dirname(__file__)
 
 scripts = ["bin/" + j for j in os.listdir("bin") ]
